87d48cd51c2f5b4e55135c83aa5777af5e09ef0a
Your Name <juliusz.mandrosz@gmail.com>
2021-08-25 09:54:33 +0200

    Added ensure initialized method to main - https://stackoverflow.com/questions/57689492/flutter-unhandled-exception-servicesbinding-defaultbinarymessenger-was-accesse

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:capybara_app/app/injection_container.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await registerDependencies();
   runApp(CapybaraApp());
 }
