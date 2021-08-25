2db99dc5a15db3a9b3cecc426b7b8e675ceb7744
Your Name <juliusz.mandrosz@gmail.com>
2021-08-25 10:44:15 +0200

    Late initializer removed form auth state

--- lib/ui/states/auth/auth_state.dart
+++ lib/ui/states/auth/auth_state.dart
@@ -6,8 +6,8 @@ import 'package:get_it/get_it.dart';
 import 'package:rxdart/subjects.dart';
 
 class AuthState with Disposable {
-  late BehaviorSubject<Token?> _token = new BehaviorSubject<Token?>();
-  late BehaviorSubject<User?> _user = new BehaviorSubject<User?>();
+  BehaviorSubject<Token?> _token = new BehaviorSubject<Token?>();
+  BehaviorSubject<User?> _user = new BehaviorSubject<User?>();
 
   BehaviorSubject<Token?> get token$ => this._token;
   BehaviorSubject<User?> get user$ => this._user;
