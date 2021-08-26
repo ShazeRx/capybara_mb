b362eec0a84052124188b386b3eb72329fd800a4
Your Name <juliusz.mandrosz@gmail.com>
2021-08-26 08:09:00 +0200

    Api urls fixed

--- lib/core/constants/api.dart
+++ lib/core/constants/api.dart
@@ -1,6 +1,6 @@
 class Api {
   static const baseUrl = 'https://cpbra-be-dev.herokuapp.com';
   static const loginUrl = '/auth/login/';
-  static const registerUrl = '/auth/register';
-  static const refreshUrl = '/auth/refresh';
+  static const registerUrl = '/auth/register/';
+  static const refreshUrl = '/auth/refresh/';
 }
