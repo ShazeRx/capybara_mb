634ddd67d267483b4b7badc51a6051221d9d27a0
Your Name <juliusz.mandrosz@gmail.com>
2021-08-29 06:17:59 +0200

    User profile provider added

---
+++ test/ui/providers/profile/user_profile_provider_test.dart
@@ -0,0 +1,126 @@
+import 'package:capybara_app/core/constants/route_paths.dart';
+import 'package:capybara_app/core/enums/provider_state.dart';
+import 'package:capybara_app/core/errors/failures/cache_failure.dart';
+import 'package:capybara_app/ui/facades/auth_facade.dart';
+import 'package:capybara_app/ui/providers/profile/user_profile_provider.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../setup/test_helpers.dart';
+
+class MockAuthFacade extends Mock implements AuthFacade {}
+
+void main() {
+  late MockAuthFacade mockAuthFacade;
+
+  late UserProfileProvider provider;
+
+  setUp(() {
+    registerThirdPartyServices();
+    mockAuthFacade = MockAuthFacade();
+    provider = UserProfileProvider(
+      authFacade: mockAuthFacade,
+    );
+  });
+
+  tearDown(() => unregisterThirdPartyServices());
+
+  void mockLogoutFailure() {
+    when(() => mockAuthFacade.logoutUser())
+        .thenAnswer((_) async => Left(CacheFailure()));
+  }
+
+  void mockLogoutSuccess() {
+    when(() => mockAuthFacade.logoutUser())
+        .thenAnswer((_) async => Right(unit));
+  }
+
+  group('on logout pressed', () {
+    test('should call logout usecase with no params', () async {
+      // Arrange
+      mockLogoutSuccess();
+
+      // Act
+      await provider.onLogoutPressed();
+
+      // Assert
+      verify(() => mockAuthFacade.logoutUser());
+
+      verifyNoMoreInteractions(mockAuthFacade);
+    });
+
+    test('should change provider state to idle after unsuccessful logout',
+        () async {
+      // Arrange
+      mockLogoutSuccess();
+
+      // Act
+      await provider.onLogoutPressed();
+      final result = provider.state;
+
+      // Assert
+      expect(result, ProviderState.idle);
+    });
+
+    test('should change provider state to idle after succesful logout',
+        () async {
+      // Arrange
+      mockLogoutSuccess();
+
+      // Act
+      await provider.onLogoutPressed();
+      final result = provider.state;
+
+      // Assert
+      expect(result, ProviderState.idle);
+    });
+
+    test('should change provider state to busy after logout attempt', () async {
+      // Arrange
+      mockLogoutSuccess();
+
+      // Act
+      provider.onLogoutPressed();
+      final result = provider.state;
+
+      // Assert
+      expect(result, ProviderState.busy);
+    });
+
+    test('should show snackbar with error after unsuccessful logout', () async {
+      // Arrange
+      mockLogoutFailure();
+
+      // Act
+      await provider.onLogoutPressed();
+
+      // Assert
+      verify(() =>
+          mockSnackbarService.showSnackbar(message: any(named: 'message')));
+    });
+
+    test('should show snackbar with error after unsuccessful logout', () async {
+      // Arrange
+      mockLogoutFailure();
+
+      // Act
+      await provider.onLogoutPressed();
+
+      // Assert
+      verify(() =>
+          mockSnackbarService.showSnackbar(message: any(named: 'message')));
+    });
+
+    test('should navigate to login screen after successful logout', () async {
+      // Arrange
+      mockLogoutSuccess();
+
+      // Act
+      await provider.onLogoutPressed();
+
+      // Assert
+      verify(() => mockNavigationService.navigateTo(RoutePaths.loginRoute));
+    });
+  });
+}
