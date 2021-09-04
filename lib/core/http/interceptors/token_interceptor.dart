4578f07b918848e588cc05bbd3eaaf7001ccf341
ShazeRx <perkowskikrzysio@gmail.com>
2021-09-04 18:49:35 +0200

    CPBRA-53 - added condition to skip adding token on refresh and login request

--- lib/core/http/interceptors/token_interceptor.dart
+++ lib/core/http/interceptors/token_interceptor.dart
@@ -1,3 +1,4 @@
+import 'package:capybara_app/core/constants/api.dart';
 import 'package:capybara_app/core/errors/exceptions/cache_exception.dart';
 import 'package:capybara_app/core/helpers/http/http_helper.dart';
 import 'package:dio/dio.dart';
@@ -8,9 +9,16 @@ class TokenInterceptor extends InterceptorsWrapper {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
+    final currentUrl = options.path;
+    final baseUrl = Api.baseUrl;
+    //Variable checks if url is login or refresh to not include token in header
+    final isLoginRequest = currentUrl == baseUrl + Api.loginUrl ||
+        currentUrl == baseUrl + Api.refreshUrl;
     try {
-      final currentToken = await HttpHelper.getCurrentToken();
-      options.headers['Authorization'] = 'Bearer ${currentToken.access}';
+      if (!isLoginRequest) {
+        final currentToken = await HttpHelper.getCurrentToken();
+        options.headers['Authorization'] = 'Bearer ${currentToken.access}';
+      }
     } on CacheException {}
     super.onRequest(options, handler);
   }
